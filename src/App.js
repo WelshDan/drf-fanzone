6abfe3e01851af2ed85912cea4453a0d7cf00f8c
WelshDan <>
2023-11-29 08:59:09 +0000

    Create handleMount and useEffect in App.js to start the process of checking if a user is logged in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container';
-import {Route,Switch} from 'react-router-dom';
-import './api/axiosDefaults';
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+import { useState } from "react";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null)
+
+  const handleMount = async () => {
+    try {
+        const { data } = await axios.get('dj-rest-auth/user')
+        setCurrentUser(data)
+    } catch(err){
+        console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    handleMount()
+  }, [])
+
   return (
     <div className={styles.App}>
       <NavBar />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import logo from '../assets/logo.png';
-import styles from '../styles/NavBar.module.css';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
             <NavLink to="/">
-            <Navbar.Brand>
-                <img src={logo} alt="logo" height="30" />
-            </Navbar.Brand></NavLink>
+                <Navbar.Brand>
+                    <img src={logo} alt="logo" height="30" />
+                </Navbar.Brand>
+            </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
@@ -20,19 +21,19 @@ const NavBar = () => {
                         className={styles.NavLink}
                         activeClassName={styles.Active}
                         to="/">
-                        <i class="fa-solid fa-house"></i>Home
+                        <i className="fa-solid fa-house"></i>Home
                     </NavLink>
                     <NavLink
                         className={styles.NavLink}
                         activeClassName={styles.Active}
                         to="/signin">
-                        <i class="fas fa-sign-in-alt"></i>Sign in
+                        <i className="fas fa-sign-in-alt"></i>Sign in
                     </NavLink>
                     <NavLink
                         className={styles.NavLink}
                         activeClassName={styles.Active}
                         to="/signup">
-                        <i class="fas fa-user-plus"></i>Sign up
+                        <i className="fas fa-user-plus"></i>Sign up
                     </NavLink>
                 </Nav>
             </Navbar.Collapse>
