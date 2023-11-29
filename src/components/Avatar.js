0ff2785d14ca4cf4cba95aa1099fe07ebd21b893
WelshDan <>
2023-11-29 13:58:23 +0000

    Add Avatar component and set its size parameters

---
+++ src/components/Avatar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "../styles/Avatar.module.css"
+
+const Avatar = ({ src, height = 45, text }) => {
+    return <span>
+        <img 
+    </span>
+}
+
+export default Avatar
