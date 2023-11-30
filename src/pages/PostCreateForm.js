e26e03cb6b4394bd918910d5b81dd89c4eb42ea3
WelshDan <>
2023-11-30 00:36:35 +0000

    Add warning messages to the form groups in PostCreateFOrm.js

--- src/pages/PostCreateForm.js
+++ src/pages/PostCreateForm.js
@@ -77,6 +77,11 @@ function PostCreateForm() {
                 onChange={handleChange}
             />
         </Form.Group>
+        {errors?.title?.map((message, idx) => (
+            <Alert key={idx} variant="warning">
+                {message}
+            </Alert>
+        ))}
         <Form.Group>
             <Form.Label>Content</Form.Label>
             <Form.Control
@@ -87,16 +92,21 @@ function PostCreateForm() {
                 onChange={handleChange}
             />
         </Form.Group>
-
+        {errors?.content?.map((message, idx) => (
+            <Alert key={idx} variant="warning">
+                {message}
+            </Alert>
+        ))}
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        onClick={() => {}}
+        onClick={() => history.goBack()}
       >
         cancel
       </Button>
       <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
         create
       </Button>
+
     </div>
   );
 
@@ -141,6 +151,11 @@ function PostCreateForm() {
                     ref={imageInput}
                 />
             </Form.Group>
+            {errors?.image?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+            ))}
             <div className="d-md-none">{textFields}</div>
           </Container>
         </Col>
