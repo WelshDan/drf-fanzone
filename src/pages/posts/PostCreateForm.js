1945b473919059730281b9fd59235b8e3d157da6
WelshDan <>
2023-12-08 01:21:39 +0000

    checked typos and missed colons in PostCreateForm

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -5,16 +5,19 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-
-import Upload from "../assets/upload.png";
-
-import styles from "../styles/PostCreateEditForm.module.css";
-import appStyles from "../App.module.css";
-import btnStyles from "../styles/Button.module.css";
-import Asset from "../components/Asset";
+import Alert from "react-bootstrap/Alert";
 import { Image } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
-import { axiosReq } from "../api/axiosDefaults";
+
+
+import Asset from "../../components/Asset";
+import Upload from "../../assets/upload.png";
+
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+import { useHistory } from "react-router";
+import { axiosReq } from "api/axiosDefaults";
 
 function PostCreateForm() {
     const [errors, setErrors] = useState({});
@@ -23,11 +26,11 @@ function PostCreateForm() {
         title: "",
         content: "",
         image: "",
-        });
+    });
     const { title, content, image } = postData;
 
-    const imageInput = useRef(null)
-    const history = useHistory()
+    const imageInput = useRef(null);
+    const history = useHistory();
 
     const handleChange = (event) => {
         setPostData({
@@ -50,65 +53,65 @@ function PostCreateForm() {
         event.preventDefault()
         const formData = new FormData();
 
-        formData.append("title", title)
-        formData.append("content", content)
-        formData.append("image", imageInput.current.files[0])
+        formData.append("title", title);
+        formData.append("content", content);
+        formData.append("image", imageInput.current.files[0]);
 
         try {
-            const {data} = await axiosReq.post("/posts/", formData);
-            history.push(`/posts/${data.id}`)
+            const { data } = await axiosReq.post("/posts/", formData);
+            history.push(`/posts/${data.id}`);
         } catch(err) {
-            console.log(err)
-            if (err.response?.status !== 401){
-                setErrors(err.response?.data)
+            console.log(err);
+            if (err.response?.status !== 401) {
+                setErrors(err.response?.data);
             }
         }
-    }
+    };
 
 
-  const textFields = (
-    <div className="text-center">
-        <Form.Group>
-            <Form.Label>Title</Form.Label>
-            <Form.Control
-                type="text"
-                name="title"
-                value={title}
-                onChange={handleChange}
-            />
-        </Form.Group>
-        {errors?.title?.map((message, idx) => (
-            <Alert key={idx} variant="warning">
-                {message}
-            </Alert>
-        ))}
-        <Form.Group>
-            <Form.Label>Content</Form.Label>
-            <Form.Control
-                as="textarea"
-                rows={6}
-                name="content"
-                value={content}
-                onChange={handleChange}
-            />
-        </Form.Group>
-        {errors?.content?.map((message, idx) => (
-            <Alert key={idx} variant="warning">
-                {message}
-            </Alert>
-        ))}
-      <Button
-        className={`${btnStyles.Button} ${btnStyles.Blue}`}
-        onClick={() => history.goBack()}
-      >
-        cancel
-      </Button>
-      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
-        create
-      </Button>
+    const textFields = (
+        <div className="text-center">
+            <Form.Group>
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="title"
+                    value={title}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors?.title?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+            ))}
+            <Form.Group>
+                <Form.Label>Content</Form.Label>
+                <Form.Control
+                    as="textarea"
+                    rows={6}
+                    name="content"
+                    value={content}
+                    onChange={handleChange}
+                />
+            </Form.Group>
+            {errors?.content?.map((message, idx) => (
+                <Alert key={idx} variant="warning">
+                    {message}
+                </Alert>
+            ))}
+            <Button
+            className={`${btnStyles.Button} ${btnStyles.Blue}`}
+            onClick={() => history.goBack()}
+            >
+            cancel
+            </Button>
+            <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+            create
+            </Button>
 
-    </div>
-  );
+        </div>
+    );
 
   return (
     <Form onSubmit={handleSubmit}>
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -6,43 +6,42 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
-import { axiosReq } from "../api/axiosDefaults";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function PostPage() {
-  const { id } = useParams();
-  const [post, setPost] = useState({ results: [] });
+    const { id } = useParams();
+    const [post, setPost] = useState({ results: [] });
 
-useEffect(() => {
-    const handleMount = async () => {
-        try {
-            const [{data: post}] = await Promise.all([
-                axiosReq.get(`/posts/${id}`),
-            ])
-            setPost({results: [post]})
-            console.log(post)
-        } catch(err){
-            console.log(err)
-        }
-    }
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const [{data: post}] = await Promise.all([
+                    axiosReq.get(`/posts/${id}`),
+                ]);
+                setPost({results: [post] });
+                console.log(post);
+            }   catch(err){
+                console.log(err);
+            }
+        };
 
-    handleMount();
-    
-})
+        handleMount();
+    }, [id]);
 
-  return (
-    <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
-        <p>Post component</p>
-        <Container className={appStyles.Content}>
-          Comments
-        </Container>
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
-      </Col>
-    </Row>
-  );
+    return (
+        <Row className="h-100">
+        <Col className="py-2 p-0 p-lg-2" lg={8}>
+            <p>Popular profiles for mobile</p>
+            <p>Post component</p>
+            <Container className={appStyles.Content}>
+            Comments
+            </Container>
+        </Col>
+        <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+            Popular profiles for desktop
+        </Col>
+        </Row>
+    );
 }
 
 export default PostPage;
