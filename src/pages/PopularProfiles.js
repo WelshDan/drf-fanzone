d77a1875c4ae563657cff781aaceee867faa22fd
WelshDan <>
2023-12-12 14:27:16 +0000

    add responsive stylings for popular profiles component

--- src/pages/PopularProfiles.js
+++ src/pages/PopularProfiles.js
@@ -1,4 +1,3 @@
-import { errors } from 'msw/lib/types/context';
 import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import appStyles from '../../App.modules.css'
@@ -7,7 +6,7 @@ import Asset from '../components/Asset';
 import { useCurrentUser } from '../contexts/CurrentUserContext';
 
 
-const PopularProfiles = () => {
+const PopularProfiles = ({ mobile }) => {
     const [profileData, setProfileData] = useState({
         pageProfile: { results: [] },
         popularProfiles: { results: [] },
@@ -34,13 +33,21 @@ const PopularProfiles = () => {
     }, [currentUser]);
 
     return (
-        <Container className={appStyles.Content}>
+        <Container className={`${appStyles.Content} ${mobile && 'd-lg-none text-center mb-3'}`}>
             {popularProfiles.results.length ? (
                 <>
                     <p>Most followed profiles.</p>
-                    {popularProfiles.results.map((profile) => (
-                        <p key={profile.id}>{profile.owner}</p>
-                    ))}
+                    {mobile ? (
+                        <div className="d-flex justify-content-around">
+                          {popularProfiles.results.slice(0,4).map((profile) => (
+                            <p key={profile.id}>{profile.owner}</p>
+                          ))}  
+                        </div>
+                    ) : (
+                        popularProfiles.results.map((profile) => (
+                            <p key={profile.id}>{profile.owner}</p>
+                        ))
+                    )}
                 </>
             ) : (
                 <Asset spinner />
