eb14b8d68240862c282bef723cedf9a77463277f
WelshDan <>
2023-12-12 09:54:45 +0000

    added linked pages to post dropdown icons

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { axiosRes } from '../../api/axiosDefaults';
 import Avatar from '../../components/Avatar';
 import { MoreDropdown } from '../../components/MoreDropdown';
@@ -27,6 +27,11 @@ const Post = (props) => {
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
+    const history = useHistory();
+
+    const handleEdit = () => {
+        history.push(`/posts/${id<}/edit`)
+    }
 
     const handleLike = async () => {
         try {
@@ -60,6 +65,15 @@ const Post = (props) => {
         }
     };
 
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/posts/${id}/`);
+            history.goBack();
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <Card className={styles.Post}>
             <Card.Body>
@@ -70,7 +84,12 @@ const Post = (props) => {
                     </Link>
                     <div className="d-flex align-items-center">
                         <span>{updated_at}</span>
-                        {is_owner && postPage && <MoreDropdown />}
+                        {is_owner && postPage && (
+                            <MoreDropdown
+                                handleEdit={handleEdit}
+                                handleDelete={handleDelete}
+                            />
+                        )}
                     </div>
                 </Media>
             </Card.Body>
